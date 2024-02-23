1545648fded4e731ce26c94d3364dff4e2310df8
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2024-02-24 00:54:39 +0100

    made the try connection function better

--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
 import requests
+import urllib
 
-def try_connection(url, port=None, path=None):
-    return True # bypassing this function that tests connection to target because it's buggy right now
-    if (port is None):
-        port = 80
-    if (path is None):
-        path = "/"
+def try_connection(url, follow_redirects=True):
     try:
-        r = requests.get(f"http://{url}:{port}{path}")
-        if (r.status_code == 200):
-            return True
+        response = requests.get(url, timeout=10, allow_redirects=follow_redirects)
+        if response.status_code in (200, 3):
+            return {"success": True, "message":"Connection successful"}
         else:
-            return False
-    except:
-        return False
+            return {"success": False, "message": f"Connection failed. Status code: {response.status_code}"}
+    except requests.ConnectionError as e:
+        return {"success": False, "message":f"Connection failed: This may be due to a bad internet network or DNS resolution issue."}
+    except requests.Timeout as e:
+        return {"success": False, "message": f"Connection timed out."}
+    except requests.RequestException as e:
+        return {"success":False, "message": f"An error occurred during the request: {str(e)}"}
     
     
 def print_banner() -> None:
@@ -25,3 +25,15 @@ def print_banner() -> None:
 | |\  || | | | | |   | |\  \ \_/ / | |   | | | | | | | |/ /| |\ \| | | |
 \_| \_/\_| |_/ \_/   \_| \_/\___/  \_/   \_| |_/ \_/ |___/ \_| \_\_| |_/
     """)
+    
+if __name__ == "__main__":
+    print_banner()
+    url1 = "https://google.com/"
+    url2 = "http://example.com"
+    url3 = "http://qwdvqvqrevwerc.com"
+    url4 = "https://twitter.com/unexistingpageervwerwer"
+    print(try_connection(url1))
+    print(try_connection(url2))
+    print(try_connection(url3))
+    print(try_connection(url4))
+    
